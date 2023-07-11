2c7f0d83499cb19ada044885446144fc01b5ad7f
MAriana Olivas <olivasgarcia031096@gmail.com>
2023-07-10 23:47:15 -0500

    modulo news

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule }  from '@nestjs/config'
 import { EnvCongifuration } from './config/app.config';
 import { AuthModule } from './auth/auth.module';
 import { NoticesModule } from './notices/notice.module';
+import { NewsModule } from './news/new.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { NoticesModule } from './notices/notice.module';
     MongooseModule.forRoot(process.env.MONGODB),
     AuthModule,
     NoticesModule,
+    NewsModule
   ],
   controllers: [
 
